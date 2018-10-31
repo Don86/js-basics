d5212846d175bfc9f17f61658f9889d089878efd
Don86 <dh.teng.86@gmail.com>
2018-10-31 15:00:15 +1100

    added more stuff to index.js from part2 video

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+//Init variables
 const userScore = 0;
 const compScore = 0;
+
+// underscore defines DOM elements
+// Caching the DOM:
+// "caching" in general means "storing something for short-term future use"
+// Caching the DOM just means storing the resuts of .getElementById in JS variables.
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_span = document.getElementById("score-board");
@@ -8,12 +14,42 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-rock_div.addEventListener("click", function() {
-    console.log("You clicked on rock.")
-})
-paper_div.addEventListener("click", function() {
-    console.log("You clicked on paper.")
-})
-scissors_div.addEventListener("click", function() {
-    console.log("You clicked on scissors.")
-})
+
+function getComputerChoice() {
+    // Define array of choices (strings, looks like)
+    const choices = ["r", "p", "s"];
+    // pick random choice with the help of the .random method, from the `Math` library
+    // note that Math.random() ~ U(0, 1)
+    // Math.floor then rounds down to nearest integer
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    return choices[randomNumber];
+}
+
+
+function game(userChoice) {
+    // prints var userChoice to the console
+    const computerChoice = getComputerChoice();
+    console.log("computer choice = "+computerChoice)
+    console.log("user option = "+userChoice)
+}
+
+function main() {
+    // Encapsulate all addEventListener methods in one function main()
+    // not sure why
+    rock_div.addEventListener('click', function() {
+        game("r");
+        //console.log("You clicked on rock.");
+    })
+    paper_div.addEventListener("click", function() {
+        game("p");
+        //console.log("You clicked on paper.");
+    })
+    scissors_div.addEventListener("click", function() {
+        game("s");
+        //console.log("You clicked on scissors.");
+    })
+}
+
+// run main()
+main();
