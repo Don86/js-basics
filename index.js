bcee86aae0b994976618c29c752bb12891ff3fdb
Don86 <dh.teng.86@gmail.com>
2018-10-31 18:29:57 +1100

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-//Init variables
-const userScore = 0;
-const compScore = 0;
+// Init variables
+// Note: a var (or const?) declared in a global scope is accessible by any function. 
+// Don't know if there are any access-modifiers. For now, just be disciplined about global and private vars.
+var userScore = 0;
+var compScore = 0;
 
 // underscore defines DOM elements
 // Caching the DOM:
@@ -9,7 +11,7 @@ const compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_span = document.getElementById("score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -26,12 +28,55 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors"
+}
+
+function userWin(userChoice, computerChoice) {
+    userScore++;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
+}
+
+function userLose(userChoice, computerChoice) {
+    compScore++;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You lose!`;
+}
+
+function userDraw(userChoice, computerChoice) {
+    result_p.innerHTML = "Draw!";
+}
 
 function game(userChoice) {
     // prints var userChoice to the console
     const computerChoice = getComputerChoice();
-    console.log("computer choice = "+computerChoice)
-    console.log("user option = "+userChoice)
+
+    switch (userChoice + computerChoice) {
+        // user win conditions
+        case "rs":
+        case "sp":
+        case "pr":
+        userWin(userChoice, computerChoice);
+        break;
+
+        // user loss conditions
+        case "rp":
+        case "sr":
+        case "ps":
+        userLose(userChoice, computerChoice);
+        break;
+
+        // draw conditions
+        case "rr":
+        case "ss":
+        case "pp":
+        userDraw(userChoice, computerChoice);
+        break;
+    }
+
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
 }
 
 function main() {
